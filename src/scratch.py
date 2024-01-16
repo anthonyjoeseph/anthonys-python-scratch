bc691ed764c0c6ac9ce0303ddd89a7e8032cce5f
Anthony Gabriele <anthonyjoeseph@gmail.com>
2024-01-16 16:42:02 -0500

    coding challenge

--- src/scratch.py
+++ src/scratch.py
@@ -1,3 +1,37 @@
-from funcy import flatten
 
-print(list(flatten([3, 2, [1, 2, 3]]))[:2])
+def decode(message_file):
+    file = open(message_file, "r")
+
+    lines = file.read().split('\n')
+
+    file.close()
+
+    pairs = list(
+        map(
+            lambda l: [
+                int(
+                    l.split(' ')[0]
+                ),
+                l.split(' ')[1].rstrip('\n')
+            ],
+            lines
+        )
+    )
+
+    pairs.sort(key=lambda l: l[0])
+
+    final = ''
+
+    interval = 2
+    index = 0
+    while index < len(pairs):
+        if final != '':
+            final += ' '
+        final += pairs[index][1]
+        index += interval
+        interval += 1
+
+    return final
+
+
+print(decode('src/data.txt'))
